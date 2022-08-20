b6f2a6cfb95d41d081e95f4ade9a43d26e286154
chyanju <chyanju@gmail.com>
2022-08-20 12:55:17 -0700

    WIP: new grammar

---
+++ picus/optimizers/r1cs-z3-simple-optimizer.rkt
@@ -0,0 +1,219 @@
+#lang rosette
+; this contains a list of simple and basic optimization steps
+;   - add p related definition and replace p
+;   - remove *1 in mul
+;   - remove +0 in add
+;   - remove -0 in sub
+;   - rewrite *x as x
+;   - rewrite -x as -x
+;   - rewrite +x as x
+;   - replace x0 with 1
+;   - rewrite 0 = -x mod p to 0 = x mod p (also -x = 0)
+;   - partial evaluation: compute concrete results, e.g., 0*0 => 0
+;     - (fixme) current only modify 0*...*x to 0
+(require
+    (prefix-in tokamak: "../tokamak.rkt")
+    (prefix-in utils: "../utils.rkt")
+    (prefix-in config: "../config.rkt")
+    (prefix-in r1cs: "../r1cs-grammar.rkt")
+)
+(provide (rename-out
+    [optimize-r1cs optimize-r1cs]
+))
+
+(define (is-rint-zero x)
+    (if (r1cs:rint? x)
+        (if (= 0 (r1cs:rint-v x))
+            #t
+            #f
+        )
+        #f
+    )
+)
+
+(define (is-rint-one x)
+    (if (r1cs:rint? x)
+        (if (= 1 (r1cs:rint-v x))
+            #t
+            #f
+        )
+        #f
+    )
+)
+
+(define (contains-rint-zero l)
+    (if (null? l)
+        #f
+        (let ([x (car l)])
+            (if (r1cs:rint? x)
+                (if (= 0 (r1cs:rint-v x))
+                    #t
+                    (contains-rint-zero (cdr l))
+                )
+                (contains-rint-zero (cdr l))
+            )
+        )
+    )
+)
+
+(define (optimize-r1cs arg-r1cs)
+    (match arg-r1cs
+
+        ; command level
+        [(r1cs:rcmds (list (r1cs:rlogic v0) vs ...))
+            (r1cs:rcmds
+                (append
+                    (list (r1cs:rlogic v0))
+                    ; add p definition
+                    (list
+                        (r1cs:rdef (r1cs:rvar "p") (r1cs:rtype "Int"))
+                        (r1cs:rassert (r1cs:req (r1cs:rvar "p") (r1cs:rint config:p)))
+                    )
+                    (for/list ([v vs]) (optimize-r1cs v))
+                )
+            )
+        ]
+        [(r1cs:rcmds vs) (r1cs:rcmds
+            (append
+                (list
+                    (r1cs:rdef (r1cs:rvar "p") (r1cs:rtype "Int"))
+                    (r1cs:rassert (r1cs:req (r1cs:rvar "p") (r1cs:rint config:p)))
+                )
+                (for/list ([v vs]) (optimize-r1cs v))
+            )
+        )]
+
+        [(r1cs:rlogic v) (r1cs:rlogic (optimize-r1cs v))]
+        ; (note) don't optimize declaration line
+        [(r1cs:rdef v t) (r1cs:rdef v (optimize-r1cs t))]
+        [(r1cs:rassert v) (r1cs:rassert (optimize-r1cs v))]
+        [(r1cs:rcmt v) (r1cs:rcmt (optimize-r1cs v))]
+        [(r1cs:rsolve ) (r1cs:rsolve )]
+
+        ; sub-command level
+        [(r1cs:req lhs rhs)
+            ; post order
+            (define new-lhs (optimize-r1cs lhs))
+            (define new-rhs (optimize-r1cs rhs))
+            (cond
+                [(&& (is-rint-zero new-lhs) (r1cs:rmod? new-rhs))
+                    (if (r1cs:rneg? (r1cs:rmod-v new-rhs))
+                        ; remove neg
+                        (r1cs:req new-lhs (r1cs:rmod (r1cs:rneg-v (r1cs:rmod-v new-rhs)) (r1cs:rmod-mod new-rhs)))
+                        ; else do nothing
+                        (r1cs:req new-lhs new-rhs)
+                    )
+                ]
+                [(&& (is-rint-zero new-rhs) (r1cs:rmod? new-lhs))
+                    (if (r1cs:rneg? (r1cs:rmod-v new-lhs))
+                        ; remove neg
+                        (r1cs:req (r1cs:rmod (r1cs:rneg-v (r1cs:rmod-v new-lhs)) (r1cs:rmod-mod new-lhs)) new-rhs)
+                        ; else do nothing
+                        (r1cs:req new-lhs new-rhs)
+                    )
+                ]
+                [else (r1cs:req new-lhs new-rhs)]
+            )
+        ]
+        [(r1cs:rneq lhs rhs) (r1cs:rneq (optimize-r1cs lhs) (optimize-r1cs rhs))]
+        [(r1cs:rleq lhs rhs) (r1cs:rleq (optimize-r1cs lhs) (optimize-r1cs rhs))]
+        [(r1cs:rlt lhs rhs) (r1cs:rlt (optimize-r1cs lhs) (optimize-r1cs rhs))]
+        [(r1cs:rgeq lhs rhs) (r1cs:rgeq (optimize-r1cs lhs) (optimize-r1cs rhs))]
+        [(r1cs:rgt lhs rhs) (r1cs:rgt (optimize-r1cs lhs) (optimize-r1cs rhs))]
+
+        [(r1cs:rand vs)
+            (define new-vs (for/list ([v vs]) (optimize-r1cs v)))
+            ; if there's only one element, extract content directly
+            (if (= 1 (length new-vs))
+                (car new-vs)
+                (r1cs:rand (for/list ([v new-vs]) v))
+            )
+        ]
+        [(r1cs:ror vs)
+            (define new-vs (for/list ([v vs]) (optimize-r1cs v)))
+            ; if there's only one element, extract content directly
+            (if (= 1 (length new-vs))
+                (car new-vs)
+                (r1cs:ror (for/list ([v vs]) v))
+            )
+        ]
+
+        [(r1cs:rint v)
+            (cond
+                ; replace as p
+                [(= config:p v) (r1cs:rvar "p")]
+                [(= (- config:p 1) v) (r1cs:rsub (list (r1cs:rvar "p") (r1cs:rint 1)))]
+                [(= (- config:p 2) v) (r1cs:rsub (list (r1cs:rvar "p") (r1cs:rint 2)))]
+                [(= (- config:p 3) v) (r1cs:rsub (list (r1cs:rvar "p") (r1cs:rint 3)))]
+                [(= (- config:p 4) v) (r1cs:rsub (list (r1cs:rvar "p") (r1cs:rint 4)))]
+                [(= (- config:p 5) v) (r1cs:rsub (list (r1cs:rvar "p") (r1cs:rint 5)))]
+                ; do nothing
+                [else (r1cs:rint v)]
+            )
+        ]
+        [(r1cs:rstr v) (r1cs:rstr v)]
+        ; (note) we assume "x0" is the first wire with prefix "x"
+        [(r1cs:rvar v)
+            (cond
+                [(equal? "x0" v) (r1cs:rint 1)]
+                [else (r1cs:rvar v)]
+            )
+        ]
+        [(r1cs:rtype v) (r1cs:rtype v)]
+
+        [(r1cs:radd vs)
+            ; remove 0
+            (define new-vs (filter
+                (lambda (x) (! (is-rint-zero x)))
+                (for/list ([v vs]) (optimize-r1cs v))
+            ))
+            ; (printf "radd new-vs is: ~a\n" new-vs)
+            (cond
+                ; no element, all values are 0 and filtered out, return base 0
+                [(= 0 (length new-vs)) (r1cs:rint 0)]
+                ; if there's only one element, rewrite to neg
+                [(= 1 (length new-vs)) (car new-vs)]
+                ; do nothing
+                [else (r1cs:radd new-vs)]
+            )
+        ]
+        ; (fixme) this could have rewrite issues
+        [(r1cs:rsub vs)
+            ; remove 0
+            (define new-vs (filter
+                (lambda (x) (! (is-rint-zero x)))
+                (for/list ([v vs]) (optimize-r1cs v))
+            ))
+            (cond
+                ; no element, all values are 0 and filtered out, return base 0
+                [(= 0 (length new-vs)) (r1cs:rint 0)]
+                ; if there's only one element, rewrite to neg
+                [(= 1 (length new-vs)) (r1cs:rneg (car new-vs))]
+                ; do nothing
+                [else (r1cs:rsub new-vs)]
+            )
+        ]
+        [(r1cs:rmul vs)
+            (define new-vs (filter
+                (lambda (x) (! (is-rint-one x)))
+                (for/list ([v vs]) (optimize-r1cs v))
+            ))
+            ; (printf "# rmul new-vs is: ~a\n" new-vs)
+            ; (printf "# cont: ~a\n vs ~a\n" (contains-rint-zero vs) vs)
+            (cond
+                ; if there's zero already in multiplication, directly return 0
+                [(contains-rint-zero new-vs) (r1cs:rint 0)]
+                ; no element, all values are 1 and filtered out, return base 1
+                [(= 0 (length new-vs)) (r1cs:rint 1)]
+                ; if there's only one element, extract content directly
+                [(= 1 (length new-vs)) (car new-vs)]
+                ; do nothing
+                [else (r1cs:rmul new-vs)]
+            )
+        ]
+        [(r1cs:rneg v) (r1cs:rneg (optimize-r1cs v))]
+        [(r1cs:rmod v mod) (r1cs:rmod (optimize-r1cs v) (optimize-r1cs mod))]
+
+        [else (tokamak:exit "not supported: ~a" arg-r1cs)]
+    )
+)
--- test-z3-uniqueness.rkt
+++ test-z3-uniqueness.rkt
@@ -3,9 +3,13 @@
 (require json rosette/solver/smt/z3
     (prefix-in tokamak: "./picus/tokamak.rkt")
     (prefix-in utils: "./picus/utils.rkt")
+    (prefix-in z3: "./picus/z3-utils.rkt")
     (prefix-in config: "./picus/config.rkt")
-    (prefix-in r1cs: "./picus/r1cs.rkt")
+    (prefix-in r1cs: "./picus/r1cs-grammar.rkt")
     (prefix-in rint: "./picus/r1cs-z3-interpreter.rkt")
+    (prefix-in parser: "./picus/r1cs-parser.rkt")
+    (prefix-in osimp: "./picus/optimizers/r1cs-z3-simple-optimizer.rkt")
+    (prefix-in oab0: "./picus/optimizers/r1cs-z3-AB0-optimizer.rkt")
 )
 
 ; stateful variable
@@ -39,82 +43,23 @@
 (printf "# r1cs file: ~a\n" arg-r1cs)
 (printf "# timeout: ~a\n" arg-timeout)
 
-; solving component
-(define (do-solve smt-str timeout #:verbose? [verbose? #f])
-    (define temp-folder (find-system-path 'temp-dir))
-    (define temp-file (format "picus~a.smt"
-        (string-replace (format "~a" (current-inexact-milliseconds)) "." "")))
-    (define temp-path (build-path temp-folder temp-file))
-    (define smt-file (open-output-file temp-path))
-    (display smt-str smt-file)
-    (close-output-port smt-file)
-    (when (|| verbose? arg-smt)
-        (printf "# written to: ~a\n" temp-path)
-    )
-
-    (when verbose?
-        (printf "# solving...\n")
-    )
-    (define-values (sp out in err)
-            ; (note) use `apply` to expand the last argument
-            (apply subprocess #f #f #f (find-executable-path "z3") (list temp-path))
-    )
-    (define engine0 (engine (lambda (_)
-        (define out-str (port->string out))
-        (define err-str (port->string err))
-        (close-input-port out)
-        (close-output-port in)
-        (close-input-port err)
-        (subprocess-wait sp)
-        (cons out-str err-str)
-    )))
-    (define eres (engine-run timeout engine0))
-    (define esol (engine-result engine0))
-    (cond
-        [(! eres)
-            ; need to kill the process
-            (subprocess-kill sp #t)
-            (cons 'timeout "")
-        ]
-        [else
-            (define out-str (car esol))
-            (define err-str (cdr esol))
-            (when verbose?
-                (printf "# stdout:\n~a\n" out-str)
-                (printf "# stderr:\n~a\n" err-str)
-            )
-            (cond
-                [(non-empty-string? err-str) (cons 'error err-str)] ; something wrong, not solved
-                [(string-prefix? out-str "unsat") (cons 'unsat out-str)]
-                [(string-prefix? out-str "sat") (cons 'sat out-str)]
-                [(string-prefix? out-str "unknown") (cons 'unknown out-str)]
-                [else (cons 'else out-str)]
-            )
-        ]
-    )
-)
-
+; load r1cs binary
 (define r0 (r1cs:read-r1cs arg-r1cs))
 (define nwires (r1cs:get-nwires r0))
 (printf "# number of wires: ~a\n" nwires)
 (printf "# number of constraints: ~a\n" (r1cs:get-mconstraints r0))
 (printf "# field size (how many bytes): ~a\n" (r1cs:get-field-size r0))
 
-(printf "# interpreting original r1cs...\n")
-(define-values (xlist original-raw) (rint:interpret-r1cs r0 null)) ; interpret the constraint system
+; parse original r1cs
+(printf "# parsing original r1cs...\n")
+(define-values (xlist original-cmds) (parser:parse-r1cs r0 null)) ; interpret the constraint system
 (define input-list (r1cs:r1cs-inputs r0))
 (define output-list (r1cs:r1cs-outputs r0))
 (printf "# inputs: ~a.\n" input-list)
 (printf "# outputs: ~a.\n" output-list)
 (printf "# xlist: ~a.\n" xlist)
-; (printf "# original-raw ~a\n" original-raw)
 
-; fix inputs, create alternative outputs
-; =======================================
-; output verification (weak verification)
-; clara fixed version
-;   |- create alternative variables for all non-input variables
-;   |- but restrict output variables as weak verification states
+; parse alternative r1cs
 (define xlist0 (for/list ([i (range nwires)])
     (if (not (utils:contains? input-list i))
         (format "y~a" i)
@@ -122,24 +67,55 @@
     )
 ))
 (printf "# xlist0: ~a.\n" xlist0)
-; then interpret again
-(printf "# interpreting alternative r1cs...\n")
-(define-values (_ alternative-raw) (rint:interpret-r1cs r0 xlist0))
-; existence of different valuation of outputs
-(define tmp-diff (for/list ([i (range nwires)])
-    (if (utils:contains? output-list i) (format "(not (= ~a ~a))" (list-ref xlist i) (list-ref xlist0 i)) null)))
-(define diff-list (filter (lambda (x) (! (null? x))) tmp-diff))
-(define diff-raw (list
-    (string-join diff-list "\n  " #:before-first "(assert (or \n  " #:after-last "))\n")
+(printf "# parsing alternative r1cs...\n")
+(define-values (_ alternative-cmds) (parser:parse-r1cs r0 xlist0))
+
+; assemble solve query
+(printf "# assembling query...\n")
+(define tmp0 (for/list ([i (range nwires)])
+    (if (utils:contains? output-list i)
+        (r1cs:rassert (r1cs:rneq (r1cs:rvar (list-ref xlist i)) (r1cs:rvar (list-ref xlist0 i))))
+        null
+    )
 ))
-; (printf "# diff-raw is:\n~a\n" diff-raw)
+(define query-cmds (r1cs:rcmds (filter (lambda (x) (! (null? x))) tmp0)))
 ; =======================================
 
-(define final-raw (append (list "(set-logic QF_NIA)\n") original-raw (list "") alternative-raw (list "") diff-raw (list "(check-sat)\n(get-model)")))
-(define final-str (string-join final-raw "\n"))
+(printf "# assembling final smt...\n")
+(define final-cmds (r1cs:append-rcmds
+    (r1cs:rcmds (list (r1cs:rlogic (r1cs:rstr "QF_NIA"))))
+    (r1cs:rcmds (list
+        (r1cs:rcmt (r1cs:rstr "================================"))
+        (r1cs:rcmt (r1cs:rstr "======== original block ========"))
+        (r1cs:rcmt (r1cs:rstr "================================"))
+    ))
+    original-cmds
+    (r1cs:rcmds (list
+        (r1cs:rcmt (r1cs:rstr "==================================="))
+        (r1cs:rcmt (r1cs:rstr "======== alternative block ========"))
+        (r1cs:rcmt (r1cs:rstr "==================================="))
+    ))
+    alternative-cmds
+    (r1cs:rcmds (list
+        (r1cs:rcmt (r1cs:rstr "============================="))
+        (r1cs:rcmt (r1cs:rstr "======== query block ========"))
+        (r1cs:rcmt (r1cs:rstr "============================="))
+    ))
+    query-cmds
+    (r1cs:rcmds (list (r1cs:rsolve )))
+))
+; perform optimization
+(define optimized-cmds
+    ; final-cmds
+    ; (osimp:optimize-r1cs final-cmds)
+    (oab0:optimize-r1cs (osimp:optimize-r1cs final-cmds))
+)
+; (define optimized-cmds final-cmds)
+(define final-str (string-join (rint:interpret-r1cs optimized-cmds) "\n"))
+(printf "# final str is:\n~a\n" final-str)
 
 ; solve!
-(define res (do-solve final-str arg-timeout))
+(define res (z3:solve final-str arg-timeout #:output-smt? arg-smt))
 (if (equal? 'unsat (car res))
     (printf "# verified.\n")
     (printf "# failed / reason: ~a\n" res)
