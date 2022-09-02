b7d6d0c462430e263f17640c8c15d20234af7e25
chyanju <chyanju@gmail.com>
2022-09-02 13:46:25 -0700

    update batch run scripts

---
+++ batch-run-ecne.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# usage: this.sh <timeout> <ecne_path> <log_path> <target_folder>
+# example: ./batch-run-ecne.sh 600000 ../EcneProject/ ./logs/ecne-circomlib ./benchmarks/circomlib-cff5ab6/
+
+otime=$1
+ecnepath=$2
+logpath=$3
+targetfolder=$4
+
+mkdir -p ${logpath}
+
+for fp in ${targetfolder}/*.r1cs
+do
+	fn=$(basename ${fp})
+	bn="${fn%.*}"
+    bp="${fp%.*}"
+	echo "=================== checking: ${fn} ==================="
+    timeout ${otime} julia --project=${ecnepath} ${ecnepath}/src/Ecne.jl --r1cs ${fp} --name ooo --sym ${bp}.sym > ${logpath}/${bn}.log 2>&1
+done
--- batch-run-picus.sh
+++ batch-run-picus.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+# usage: this.sh <timeout> <log_path> <target_folder>
+# example: ./batch-run-picus.sh 600000 ./logs/test/ ./benchmarks/circomlib-cff5ab6/
 
 otime=$1
-targetfolder=$2
+logpath=$2
+targetfolder=$3
 
+mkdir -p ${logpath}
 
 for fp in ${targetfolder}/*.r1cs
 do
 	fn=$(basename ${fp})
 	bn="${fn%.*}"
+    bp="${fp%.*}"
 	echo "=================== checking: ${fn} ==================="
-    timeout ${otime} racket ./test-pp-uniqueness.rkt --timeout 5000 --solver cvc5 --initlvl 0 --weak --r1cs ${fp} > ./logs/${bn}.log 2>&1
+    timeout ${otime} racket ./test-pp-uniqueness.rkt --timeout 5000 --solver cvc5 --initlvl 0 --weak --r1cs ${fp} > ${logpath}/${bn}.log 2>&1
 done
