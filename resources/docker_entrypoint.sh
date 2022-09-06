32de137676579adbf21c71d328947f58bbce92f0
Bryan Tan <bryan@veridise.com>
2022-09-06 15:40:36 -0700

    Add docker_entrypoint.sh variables to make running easier

--- resources/docker_entrypoint.sh
+++ resources/docker_entrypoint.sh
@@ -1,27 +1,45 @@
 #!/usr/bin/env bash
 
-set -e
+# Script used for invoking Picus from Docker.
+# Instructions: set the following environment variables before calling this
+# script, then run this script with no arguments.
 
-CIRCOM_SRC_DIR=${CIRCOM_SRC_DIR:-/src_files}
-CIRCOM_OUT_DIR=${CIRCOM_OUT_DIR:-/artifact_dir}
+# The folder containing all source files, including libraries.
+CIRCOM_SRC_DIR="${CIRCOM_SRC_DIR:-/src_files}"
+
+# A colon-separated list of folders/files in CIRCOM_SRC_DIR that Picus should
+# be run on.
+MAIN_FILES="${MAIN_FILES:-${CIRCOM_SRC_DIR}}"
+
+# The folder where the circom output and picus logs should be stored.
+CIRCOM_OUT_DIR="${CIRCOM_OUT_DIR:-/artifact_dir}"
+
+set -e
 
 mkdir -p "$CIRCOM_OUT_DIR"
 
 # Run circom compiler
 echo "Running circom compiler..."
-for f in "$CIRCOM_SRC_DIR"/*.circom; do
-    echo "Compile: $f"
-    circom -o "${CIRCOM_OUT_DIR}" "$f" --r1cs --sym --O0
+for main_loc in $(xargs -n1 -d: <<< "$MAIN_FILES"); do
+    for f in $(find "$main_loc" -type f -name '*.circom'); do
+        echo "Compile: $f"
+        out_dir=$(dirname $(realpath --relative-to="$CIRCOM_SRC_DIR" "$f"))
+        mkdir -p "${CIRCOM_OUT_DIR}"/"$out_dir"
+        circom -o "${CIRCOM_OUT_DIR}"/"$out_dir" "$f" --r1cs --sym --O0
+    done
 done
 echo "Done compiling."
 echo
 
 # Run uniqueness tests on r1cs files
 echo "Running picus..."
-for f in "$CIRCOM_OUT_DIR"/*.r1cs; do
+for f in $(find "$CIRCOM_OUT_DIR" -type f -name '*.r1cs'); do
     echo "--------------------------------------------------------"
     echo "Running picus on: $f"
-    racket test-pp-uniqueness.rkt --r1cs "$f" --solver cvc5 --weak
+    fdir=$(dirname "$f")
+    logfile="$fdir"/"$(basename "$f")".picus.log
+    racket test-pp-uniqueness.rkt --r1cs "$f" --solver cvc5 --weak |& tee "$logfile"
+    echo "Log saved to $logfile"
     echo "--------------------------------------------------------"
     echo
 done
