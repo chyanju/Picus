fc7bd273e0179a4ae2096bff4f2c83da5957f792
chyanju <chyanju@gmail.com>
2023-02-25 12:37:23 -0800

    Update picus-compile.sh

--- picus-compile.sh
+++ picus-compile.sh
@@ -7,5 +7,5 @@ fd="$(dirname "${fp}")"
 fn="$(basename "${fp}")"
 bn="${fn%.*}"
 
-echo "# preparing: ${fp}"
-circom -o ${fd} ${fp} --r1cs --sym --O0
+# echo "# preparing: ${fp}"
+circom -o ${fd} ${fp} --r1cs --sym --O0 > /dev/null 2>&1
